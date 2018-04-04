085bc1caf5e8877982e09fed1657c2cbda4228a4
EvaFro <frolli.erin@gmail.com>
2018-04-04 11:23:36 -0700

    install webpack & npm library

---
+++ challenge_1/webpack.config.js
@@ -0,0 +1,25 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'public');
+var SRC_DIR = path.resolve(__dirname, 'client');
+
+var config = {
+  entry: SRC_DIR + '/app.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  loaders : [
+    {
+      test : /\.jsx?/,
+      include : SRC_DIR,
+      loader : 'babel-loader',      
+      query: {
+        presets: ['react', 'es2015']
+     }
+    }
+  ]
+};
+
+module.exports = config;
