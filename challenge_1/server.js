3392fa7dbf3cad323373ede609d54e3846750a7b
EvaFro <frolli.erin@gmail.com>
2018-04-04 11:40:50 -0700

    fill out server & add scripts to package.json

--- challenge_1/server.js
+++ challenge_1/server.js
@@ -1,2 +1,11 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var App = express();
 
+App.use(bodyParser.json());
 
+App.use(express.static(__dirname + '/public'));
+
+App.listen(3000, function() {
+  console.log(`listening on port: localHost:3000!`);
+});
