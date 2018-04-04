2736971ee429f97e37304671e774e731cc927840
EvaFro <frolli.erin@gmail.com>
2018-04-04 12:01:08 -0700

    edit index & server to ensure component renders

--- challenge_1/server.js
+++ challenge_1/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var App = express();
 
-App.use(bodyParser.json());
+// App.use(bodyParser.json());
 
 App.use(express.static(__dirname + '/public'));
 
