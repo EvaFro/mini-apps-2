68eac5226f73eac7f5d990ed3d8a1102880192c3
EvaFro <frolli.erin@gmail.com>
2018-04-05 11:50:58 -0700

    add shopping cart button to homepage

--- challenge_2/client/homePage.jsx
+++ challenge_2/client/homePage.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 
 const HomePage = () => (
   <div>
-        <h1>Computer Supplies R Us</h1>
-        <br/>
-        <p>We have all your computer supply needs.</p>
-      </div>
+    <h1>Computer Supplies R Us</h1>
+    <br/>
+    <p>We have all your computer supply needs.</p>
+    <br/>
+    <br/>
+    <button type="button">Shopping Cart</button>
+  </div>
 )
 
 export default HomePage;
