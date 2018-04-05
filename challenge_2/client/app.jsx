05def8a1231e175c8befac5aecfa15d964eb1a63
EvaFro <frolli.erin@gmail.com>
2018-04-05 11:53:17 -0700

    rearanged client folder & updated import links

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
-import ShoppingCart from './shoppingCart.jsx';
+import ShoppingCart from './components/shoppingCart.jsx';
 
 class App extends React.Component {
   render () {
