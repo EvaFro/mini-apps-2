05fa328d111aa1a0c42ada7b0db5705b7696d595
EvaFro <frolli.erin@gmail.com>
2018-04-05 11:40:18 -0700

    edit shoppingCart layout

--- challenge_2/client/shoppingCart.jsx
+++ challenge_2/client/shoppingCart.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 
 const ShoppingCart = () => (
   <div>
-        <h1>Proceed to checkout.</h1>
+        <h1>Please proceed to checkout...</h1>
         <br/>
         <h3>Shopping Cart</h3>
+        <li>Computer Stand.............$30</li>
+        <li>Bluetooth Mouse...........$45</li>
+        <li>Bluetooth Keyboard......$50</li>
+        <li>Computer Cover............$25</li>
+        <li>Computer Bag...............$30</li>
+        <li>Accesories Bag.............$15</li>
         <br/>
-        <li>Computer Stand       $30</li>
-        <li>Bluetooth Mouse      $45</li>
-        <li>Bluetooth Keyboard   $50</li>
-        <li>Computer Cover       $25</li>
-        <li>Computer Bag         $30</li>
-        <li>Accesories Bag       $15</li>
-        <li>Totoal               $195</li>
+        <div><b>Totoal: $195</b></div>
         <br/>
         <br/>
         <button type="button">Checkout</button>
