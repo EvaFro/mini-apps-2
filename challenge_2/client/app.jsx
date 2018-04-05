1cdbb26c9a9622b68c58fdcf1c216e6e2b9a065d
EvaFro <frolli.erin@gmail.com>
2018-04-05 10:49:39 -0700

    create webpack & start starter files

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,2 +1,11 @@
+import React from 'react';
+import {render} from 'react-dom';
 
+class App extends React.Component {
+  render () {
+    return <h1>Buy Products at Frolli Studios</h1>;
+  }
+}
+
+render(<App/>, document.getElementById('app'));
 
