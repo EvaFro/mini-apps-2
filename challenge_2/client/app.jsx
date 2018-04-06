8f6fb6b6406675c4d77091c8ddc2a6f9ea8fa93a
EvaFro <frolli.erin@gmail.com>
2018-04-05 19:06:32 -0700

    add conditional rendering to app

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,12 +1,49 @@
 import React from 'react';
 import {render} from 'react-dom';
 import ShoppingCart from './components/shoppingCart.jsx';
+import HomePage from './components/homePage.jsx'
+import Form1 from './components/form1.jsx'
+import Form2 from './components/form2.jsx'
+import Form3 from './components/form3.jsx'
 
 class App extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      currentComponent: 'homepage'
+    }
+  }
+
+  switchComponent(name) {
+    console.log('This is the current Component: ', name)
+    this.setState({
+      currentComponent: name
+    });
+  }
+
+  displayPage(){
+    let page = this.state.currentComponent
+    switch(page) {
+      case 'homepage':
+        return <HomePage switchComponent={name => this.switchComponent(name)}/>
+      case 'shoppingCart':
+        return <ShoppingCart switchComponent={name => this.switchComponent(name)}/>
+      case 'user':
+        return <Form1 switchComponent={name => this.switchComponent(name)}/>
+      case 'payment':
+        return  <Form3 switchComponent={name => this.switchComponent(name)}/>
+      case 'shipping':
+        return <Form2 switchComponent={name => this.switchComponent(name)}/>
+    }
+  }
+
   render () {
     return (
       <div>
-        <ShoppingCart />
+        {
+          this.displayPage()
+        }
       </div>
     )
   }
