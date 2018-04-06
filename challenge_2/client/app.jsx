395cfd55759cba40998c196235325d29b0251fb0
EvaFro <frolli.erin@gmail.com>
2018-04-06 08:07:27 -0700

    add the summaryPage to the app

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -5,20 +5,27 @@ import HomePage from './components/homePage.jsx'
 import Form1 from './components/form1.jsx'
 import Form2 from './components/form2.jsx'
 import Form3 from './components/form3.jsx'
+import SummaryPage from './components/summaryPage.jsx'
 
 class App extends React.Component {
 
   constructor(props){
     super(props)
     this.state = {
-      currentComponent: 'homepage'
+      currentComponent: 'homepage',
+      user: {},
+      payment: {},
+      shipping: {}
     }
   }
 
-  switchComponent(name) {
-    console.log('This is the current Component: ', name)
+  switchComponent(data) {
+    console.log('This is the current Component: ', data.name)
     this.setState({
-      currentComponent: name
+      currentComponent: data.name,
+      user: data.user ? data.user : this.state.user,
+      payment: data.payment ? data.payment : this.state.user,
+      shipping: data.shipping ? data.shipping : this.state.shipping
     });
   }
 
@@ -26,15 +33,22 @@ class App extends React.Component {
     let page = this.state.currentComponent
     switch(page) {
       case 'homepage':
-        return <HomePage switchComponent={name => this.switchComponent(name)}/>
+        return <HomePage switchComponent={data => this.switchComponent(data)}/>
       case 'shoppingCart':
-        return <ShoppingCart switchComponent={name => this.switchComponent(name)}/>
+        return <ShoppingCart switchComponent={data => this.switchComponent(data)}/>
       case 'user':
-        return <Form1 switchComponent={name => this.switchComponent(name)}/>
+        return <Form1 switchComponent={data => this.switchComponent(data)}/>
       case 'payment':
-        return  <Form3 switchComponent={name => this.switchComponent(name)}/>
+        return  <Form3 switchComponent={data => this.switchComponent(data)}/>
       case 'shipping':
-        return <Form2 switchComponent={name => this.switchComponent(name)}/>
+        return <Form2 switchComponent={data => this.switchComponent(data)}/>
+      case 'summary':
+        return <SummaryPage 
+                switchComponent={data => this.switchComponent(data)}
+                user={this.state.user}
+                payment={this.state.payment}
+                shipping={this.state.shipping}
+                />
     }
   }
 
