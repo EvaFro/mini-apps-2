386726ad5c6d82a7d38292d814e286fa158cb7e8
EvaFro <frolli.erin@gmail.com>
2018-04-07 09:40:40 -0700

    add a middlewhere 2 display coming in urls

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var app = express();
 
+app.use('/', (req, res, next) => {
+  console.log(`${req.method} received from ${req.url}`);
+  next();
+});
+
 app.use(express.static(__dirname+'/public'));
+
+
 
 app.listen(3000, ()=> console.log('listing on port 3000'));
 
